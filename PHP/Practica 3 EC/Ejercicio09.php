67ad67155e2efccca9cd9e358bff822ffeb30f23
v1ct0rjs <vmjimenezs02@educarex.es>
2025-01-08 17:12:22 +0100

    actualizado

--- PHP/Practica 3 EC/Ejercicio09.php
+++ PHP/Practica 3 EC/Ejercicio09.php
@@ -5,22 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 09</title>
 </head>
-
 <body>
-    <form name=”prueba” method=”post” action=”Ejercicio09.php”>
-    <input type=”number” name=”Numero”/>
-    <input type=”submit” name=”enviar”/>
-</form>
+    <form name="prueba" method="post" action="Ejercicio09.php">
+        <label for="Numero">Introduce un número:</label>
+        <input type="number" id="Numero" name="Numero"><br>
+        <input type="submit" name="enviar" value="Calcular">
+    </form>
     <?php
         $numero = $_POST["Numero"];
-        $var = 1;
-        
-        while ($var <= 10){
+        echo "<h3>Tabla de multiplicar del número $numero</h3>";
+        for ($var = 1; $var <= 10; $var++) {
             $resultado = $var * $numero;
-            echo "Tabla de multiplicar del numero", $numero;
-            echo $resultado, "<br>";
-            $var ++;
-        } 
+            echo "$var &times; $numero = $resultado<br>";
+        }
     ?>
 </body>
 </html>
