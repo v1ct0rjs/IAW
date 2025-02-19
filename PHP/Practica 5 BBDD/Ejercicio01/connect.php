14cc5a3a817b2327ac98aad33800968d0707aabc
v1ct0rjs <vmjimenezs02@educarex.es>
2025-02-19 14:15:32 +0100

    conntinuamos con estrucctuda

--- PHP/Practica 5 BBDD/Ejercicio01/connect.php
+++ PHP/Practica 5 BBDD/Ejercicio01/connect.php
@@ -3,20 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Conexión</title>
 </head>
-<body>
+<body> 
     <?php
-    echo " Hemos entrado<br><br>";
-        $conn = mysqli_connect('10.0.20.37', $_POST['user'], $_POST['pass']);
-        if (!$conn) 
-        {
-            die('No pudo conectarse: '. mysqli_connect_error());
-        }
-        else
-        {
-            echo 'Conectado';
-        }
-    ?> 
+    $conn = mysqli_connect('10.0.20.37', 'v1ct0r', 'vmjimsan');
+    if (!$conn) {
+        die('No pudo conectarse: ' . mysql_error());
+    }
+    
+
+    $tabla = "CREATE TABLE IF NOT EXISTS agenda (";
+    $tabla .= "id INT NOT NULL AUTO_INCREMENT, ";
+    $tabla .= "nombre CHAR(50), ";
+    $tabla .= "direccion CHAR(100), ";
+    $tabla .= "telefono CHAR(15), ";
+    $tabla .= "email CHAR(50), ";
+    $tabla .= "KEY (id) ) ";
+
+    if (mysql_query($sql, $conn)) {
+        echo "La base de datos gestion se creó correctamente\n";
+    } else {
+        echo 'Error al crear la base de datos: ' . mysql_error() . "\n";
+    }
+    mysql_query($tabla_usuarios, $conn);
+
+    if (isset($_POST["enviar"])){
+
+    }
+    ?>
 </body>
 </html>
---
+++ PHP/Practica 5 BBDD/Ejercicio01/form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <form method="post" action="connect.php">   
+        <label for="dni">DNI</label>
+        <input type="text" name="dni" required><br>
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" required><br>
+        <label for="apellidos">Apellidos</label>
+        <input type="text" name="apellidos" required><br>
+        <label for="localidad">Localidad</label>
+        <input type="text" name="localidad" required><br>
+        <label for="centro">Centro de estudios</label>
+        <input type="text" name="centro" required><br>
+        <label for="usuario">Usuario</label>
+        <input type="text" name="usuario" required><br>
+        <label for="pass">Contraseña</label>
+        <input type="text" name="pass" required><br>
+        <input type="submit" name="enviar" value="enviar"><br><br>
+    </form>
+    
+</body>
+</html>
